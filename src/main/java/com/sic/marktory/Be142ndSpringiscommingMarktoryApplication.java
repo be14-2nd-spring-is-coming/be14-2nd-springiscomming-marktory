fd96186bb482d3129ef5ff1568679e56fb11b87c
Jooahyeon <dkgus0496@naver.com>
2025-03-18 20:50:52 +0900

    setting : 레포지토리 변경

---
+++ src/main/java/com/sic/marktory/Be142ndSpringiscommingMarktoryApplication.java
@@ -0,0 +1,14 @@
+package com.sic.marktory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//
+@SpringBootApplication
+public class Be142ndSpringiscommingMarktoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Be142ndSpringiscommingMarktoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sic/marktory/Be142ndSpringiscommingMarktoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sic.marktory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Be142ndSpringiscommingMarktoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
