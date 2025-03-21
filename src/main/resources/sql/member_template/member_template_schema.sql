db0c51945160915723453e8d4054b7081c9f5577
Morris235 <dltkdah777@gmail.com>
2025-03-21 19:14:56 +0900

    chore : 템플릿 공개 visibility속성의 default public 제거

--- src/main/resources/sql/member_template/member_template_schema.sql
+++ src/main/resources/sql/member_template/member_template_schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE member_template(
                                 id INT PRIMARY KEY AUTO_INCREMENT,
                                 title VARCHAR(255) NOT NULL,
                                 content VARCHAR(255) NOT NULL,
-                                visibility VARCHAR(255) NOT NULL DEFAULT 'public',
+                                visibility VARCHAR(255) NOT NULL,
                                 written_date VARCHAR(255) NOT NULL,
                                 delete_date VARCHAR(255),
                                 usage_count INT NOT NULL DEFAULT 0,
