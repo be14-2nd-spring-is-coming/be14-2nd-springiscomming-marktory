908d79618a50ab5b5a0d6928e09530dc4533ea97
RKDLDE <kangcanddy@gmail.com>
2025-03-21 17:28:48 +0900

    fix: 좋아요 주체 추가 (회원 ID)

--- src/main/resources/sql/likes/likes_schema.sql
+++ src/main/resources/sql/likes/likes_schema.sql
@@ -5,6 +5,10 @@ CREATE TABLE likes (
                        type VARCHAR(255) NOT NULL,
                        post_id INT,
                        comment_id INT,
+                       member_id INT NOT NULL,
                        CONSTRAINT FOREIGN KEY (post_id) REFERENCES post(id),
-                       CONSTRAINT FOREIGN KEY (comment_id) REFERENCES comment(id)
+                       CONSTRAINT FOREIGN KEY (comment_id) REFERENCES comment(id),
+                       CONSTRAINT FOREIGN KEY (member_id) REFERENCES member(id),
+                       UNIQUE (member_id, post_id, type),
+                       UNIQUE (member_id, comment_id, type)
 ) ENGINE = INNODB;
