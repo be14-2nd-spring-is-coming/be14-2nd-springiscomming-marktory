63fc8ec0ff7a72db2af9e987b5b3575b86ca2401
RKDLDE <kangcanddy@gmail.com>
2025-03-21 21:06:27 +0900

    chore: member_profile_image_schema 테이블 id 삭제

--- src/main/resources/sql/marktory_data.sql
+++ src/main/resources/sql/marktory_data.sql
@@ -51,9 +51,7 @@ CREATE TABLE subscribe (
 
 CREATE TABLE member_profile_image (
     member_id INT PRIMARY KEY,
-    id INT NOT NULL AUTO_INCREMENT,
     URL VARCHAR(255),
-    UNIQUE(id),
     CONSTRAINT FOREIGN KEY (member_id) REFERENCES member(id)
 ) ENGINE = INNODB;
 
