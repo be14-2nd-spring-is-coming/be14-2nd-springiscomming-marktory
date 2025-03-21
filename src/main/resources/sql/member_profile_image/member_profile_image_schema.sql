0b3dd47cbf50644eb605b62e010a41ed24fe4aed
RKDLDE <kangcanddy@gmail.com>
2025-03-21 20:36:26 +0900

    style: 들여쓰기

--- src/main/resources/sql/member_profile_image/member_profile_image_schema.sql
+++ src/main/resources/sql/member_profile_image/member_profile_image_schema.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS member_profile_image;
 
 CREATE TABLE member_profile_image (
-                                    member_id INT PRIMARY KEY,
-                                    id INT NOT NULL AUTO_INCREMENT,
-                                    URL VARCHAR(255),
-                                    UNIQUE(id),
-                                    CONSTRAINT FOREIGN KEY (member_id) REFERENCES member(id)
+    member_id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
+    URL VARCHAR(255),
+    UNIQUE(id),
+    CONSTRAINT FOREIGN KEY (member_id) REFERENCES member(id)
 ) ENGINE = INNODB;
