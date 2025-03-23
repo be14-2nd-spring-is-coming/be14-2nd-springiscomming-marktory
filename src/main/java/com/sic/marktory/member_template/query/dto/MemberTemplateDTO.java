89d1b8a7df91b1fd0b3fb90ec0eecbdf4b0a448c
Morris235 <dltkdah777@gmail.com>
2025-03-23 19:45:43 +0900

    test : 회원 공개 템플릿 찾기 매핑 TDD

---
+++ src/main/java/com/sic/marktory/member_template/query/dto/MemberTemplateDTO.java
@@ -0,0 +1,20 @@
+package com.sic.marktory.member_template.query.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class MemberTemplateDTO {
+    private Long id;
+    private String title;
+    private String content;
+    private String visibility;
+    private String writtenDate;
+    private String deleteDate;
+    private int usageCount;
+    private char isCopy;
+    private Long repositoryId;
+}
