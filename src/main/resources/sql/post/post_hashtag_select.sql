67a27fee181a32b4bcdca3e734a092b01d3b1f4e
Jooahyeon <dkgus0496@naver.com>
2025-03-24 06:00:19 +0900

    [FEAT] 회원 전체공개 게시글 조회 기능 개발

--- src/main/resources/sql/post/post_hashtag_select.sql
+++ src/main/resources/sql/post/post_hashtag_select.sql
@@ -1,16 +1,9 @@
-# 해시태그 id를 통한 특정 게시글 조회
+# 해시태그를 통한 특정 게시글 조회
 
-SELECT
-    B.hashtag_id,
-    C.name,
-    A.id,
-    A.title,
-    A.content,
-    A.written_date,
-    A.delete_date,
-    A.visibility,
-    A.member_id
-FROM post A
-         JOIN post_hashtag B ON A.id = B.post_id
-         JOIN hashtag C ON C.id = B.hashtag_id
-WHERE hashtag_id = ?;
+SELECT p.id, p.title, p.content, p.written_date, p.visibility, p.member_id, p.category_id
+FROM post p
+         JOIN post_hashtag ph ON p.id = ph.post_id
+         JOIN hashtag h ON ph.hashtag_id = h.id
+WHERE h.name = ? #{hashtagName}
+  AND p.visibility != 'public'
+ORDER BY p.written_date DESC;
