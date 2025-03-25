f604c5bdfce53a8cf1751b8db9187e2b0e228ecc
rainyday1367 <rainyday1367@naver.com>
2025-03-25 10:47:14 +0900

    REFACTOR AppConfig에 JavaMailSender 빈 속성 추가

--- src/main/java/com/sic/marktory/config/AppConfig.java
+++ src/main/java/com/sic/marktory/config/AppConfig.java
@@ -3,6 +3,8 @@ package com.sic.marktory.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -20,4 +22,10 @@ public class AppConfig {
         return new BCryptPasswordEncoder();
     }
 
+
+    /* 설명. JavaMailSender */
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSenderImpl();
+    }
 }
