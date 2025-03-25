ece38781e6724c3acac38ad42224f6435c10b0f7
Morris235 <dltkdah777@gmail.com>
2025-03-25 20:58:54 +0900

    docs : 각 컨트롤러에 스웨거 API 설명 어노테이션 작성

--- src/main/java/com/sic/marktory/Be142ndSpringiscommingMarktoryApplication.java
+++ src/main/java/com/sic/marktory/Be142ndSpringiscommingMarktoryApplication.java
@@ -1,9 +1,22 @@
 package com.sic.marktory;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//
+@OpenAPIDefinition(
+        info = @Info(
+                title = "MarkTory",
+                version = "v0.0.2",
+                description = "MarkTory의 API 명세서"
+        )
+//        , servers = {
+//                @Server(url = "http://api.localhost:8080", description = "blog service"),
+//                @Server(url = "http://api.member:8761", description = "member service")
+//        }
+)
 @SpringBootApplication
 public class Be142ndSpringiscommingMarktoryApplication {
 
--- src/main/java/com/sic/marktory/subscribe/query/controller/SubscribeController.java
+++ src/main/java/com/sic/marktory/subscribe/query/controller/SubscribeController.java
@@ -2,6 +2,8 @@ package com.sic.marktory.subscribe.query.controller;
 
 import com.sic.marktory.subscribe.query.dto.MemberDTO;
 import com.sic.marktory.subscribe.query.service.SubscribeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 // 조회 API
+@Tag(name = "SubScribe Query", description = "구독한 회원 조회, 내 구독자 조회")
 @RestController("subscribeQueryController")
 @RequestMapping("/api/subscribe")
 public class SubscribeController {
@@ -22,12 +25,28 @@ public class SubscribeController {
     }
 
     // 내가 구독한 회원들 조회
+    @Operation(
+            summary = "구독한 회원 조회",
+            description = """
+        특정 회원이 구독하고 있는 다른 회원들의 목록을 조회합니다.
+        - `memberId`를 기준으로 해당 사용자가 구독 중인 대상 목록을 반환합니다.
+        - 반환 타입은 `MemberDTO` 리스트입니다.
+        """
+    )
     @GetMapping("/subscriber/{memberId}")
     public List<MemberDTO> getSubscribedMembers(@PathVariable("memberId") Long memberId){
         return subscribeService.findSubscribedMembers(memberId);
     }
 
     // 나를 구독한 회원들 조회
+    @Operation(
+            summary = "내 구독자 조회",
+            description = """
+        특정 회원을 구독 중인 회원들(구독자)의 목록을 조회합니다.
+        - `memberId`를 기준으로 나를 구독 중인 회원들을 반환합니다.
+        - 반환 타입은 `MemberDTO` 리스트입니다.
+        """
+    )
     @GetMapping("/subscribed/{memberId}")
     public List<MemberDTO> getSubscriberMembers(@PathVariable("memberId") Long memberId){
         return subscribeService.findSubscriberMembers(memberId);
