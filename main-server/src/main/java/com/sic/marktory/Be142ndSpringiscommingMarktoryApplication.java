1e272a5336103a92423b9a53e32e1913ed00673c
rainyday1367 <rainyday1367@naver.com>
2025-03-25 17:38:10 +0900

    Chore Gateway와 Main-Server 연결 완료

--- main-server/src/main/java/com/sic/marktory/Be142ndSpringiscommingMarktoryApplication.java
+++ main-server/src/main/java/com/sic/marktory/Be142ndSpringiscommingMarktoryApplication.java
@@ -2,9 +2,11 @@ package com.sic.marktory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 //
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Be142ndSpringiscommingMarktoryApplication {
 
     public static void main(String[] args) {
